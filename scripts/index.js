0a29396ada81edb647a177089d9f5f39c829718e
Shahriyor <shahriyordeveloping@gmail.com>
2021-11-13 20:33:41 +0500

    initial

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,22 @@ window.addEventListener('scroll', function(e){
         });
     }
 })
+const headerButton = document.querySelector(".header__button");
+const headerMenu = document.querySelector(".header__list");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+headerButton.onclick = menuToggle;
+window.onclick = (e) => {
+    if (menuOpened && !e.composedPath().includes(headerButton) && !e.composedPath().includes(headerMenu)){
+        menuToggle();
+    }
+    if(e.target.classList.contains('header__link')){
+        setTimeout(() => {
+            menuToggle()
+        }, 100);
+    }
+};
